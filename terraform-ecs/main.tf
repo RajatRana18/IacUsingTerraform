50999619981aa9cbfaa94670fbee7133b6c269ec
Rajat Rana <rrajat18@gmail.com>
2024-05-17 20:03:06 +0530

    first commit

---
+++ terraform-ecs/main.tf
@@ -0,0 +1,90 @@
+# main.tf
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_ecs_cluster" "ecs_cluster" {
+  name = "${var.project_name}-cluster"
+}
+
+resource "aws_ecs_task_definition" "task" {
+  family                   = "${var.project_name}-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([
+    {
+      name  = "app"
+      image = "${aws_ecr_repository.repo.repository_url}:latest"
+      essential = true
+
+      portMappings = [
+        {
+          containerPort = 3000
+          hostPort      = 3000
+        }
+      ]
+    }
+  ])
+
+  execution_role_arn = aws_iam_role.execution_role.arn
+}
+
+resource "aws_ecs_service" "service" {
+  name            = "${var.project_name}-service"
+  cluster         = aws_ecs_cluster.ecs_cluster.id
+  task_definition = aws_ecs_task_definition.task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+  network_configuration {
+    subnets = var.subnets
+    security_groups = [aws_security_group.sg.id]
+  }
+}
+
+resource "aws_security_group" "sg" {
+  name        = "${var.project_name}-sg"
+  description = "Allow inbound traffic to the application"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_iam_role" "execution_role" {
+  name = "${var.project_name}-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  ]
+}
+
+resource "aws_ecr_repository" "repo" {
+  name = var.project_name
+}
